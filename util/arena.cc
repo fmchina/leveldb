7c81972966407185150e05ba753adce3bb287f58
fmchina <fmchina@126.com>
2018-01-12 21:13:03 +0800

    注释arena内存管理模块

--- util/arena.cc
+++ util/arena.cc
@@ -15,12 +15,14 @@ Arena::Arena() : memory_usage_(0) {
 }
 
 Arena::~Arena() {
+  // 释放所有内存块。
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
 }
-
+// 申请内存
 char* Arena::AllocateFallback(size_t bytes) {
+  // 如果要申请的内存大于块的1/4直接申请一块。
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
@@ -29,6 +31,7 @@ char* Arena::AllocateFallback(size_t bytes) {
   }
 
   // We waste the remaining space in the current block.
+  // 如果申请的内存比较小，先申请一块大内存保存，使用这块内存的一部分，剩余的等下次使用。
   alloc_ptr_ = AllocateNewBlock(kBlockSize);
   alloc_bytes_remaining_ = kBlockSize;
 
@@ -37,29 +40,31 @@ char* Arena::AllocateFallback(size_t bytes) {
   alloc_bytes_remaining_ -= bytes;
   return result;
 }
-
+// 申请对齐的内存
 char* Arena::AllocateAligned(size_t bytes) {
+
+  //如果指针长度小于8，按8byte对齐。
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
-  assert((align & (align-1)) == 0);   // Pointer size should be a power of 2
+  assert((align & (align-1)) == 0);   // Pointer size should be a power of 2 指针地址应该是2的指数。
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
-  size_t needed = bytes + slop;
+  size_t needed = bytes + slop; // 实际申请的内存大小，补齐
   char* result;
   if (needed <= alloc_bytes_remaining_) {
-    result = alloc_ptr_ + slop;
+    result = alloc_ptr_ + slop;// 对齐地址。
     alloc_ptr_ += needed;
     alloc_bytes_remaining_ -= needed;
   } else {
     // AllocateFallback always returned aligned memory
-    result = AllocateFallback(bytes);
+    result = AllocateFallback(bytes);// 直接申请。
   }
   assert((reinterpret_cast<uintptr_t>(result) & (align-1)) == 0);
   return result;
 }
-
+// 申请一个新块内存
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
-  blocks_.push_back(result);
+  blocks_.push_back(result); // 保存申请的新块。
   memory_usage_.NoBarrier_Store(
       reinterpret_cast<void*>(MemoryUsage() + block_bytes + sizeof(char*)));
   return result;
